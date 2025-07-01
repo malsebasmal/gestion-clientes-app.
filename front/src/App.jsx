953d07a84f2f7be9f3cedce0697aa7946b1a67f5
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-06-30 21:02:38 -0500

    add: handle for delete and post client

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,16 +4,20 @@ import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from 
 import axios from "axios"
 
 function App() {
+  const URLAPI = "http://localhost:3000/clients"
+
   const [data, setData] = useState([])
-  const [name, setName] = useState("none")
-  const [email, setEmail] = useState("none")
-  const [phoneNumber, setPhoneNumber] = useState("none")
-  const [enterprise, setEnterprise] = useState("none")
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    enterprise: ""
+  })
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/clients')
+        const response = await axios.get(URLAPI)
         setData(response.data)
       } catch (error) {
         console.error(error.message);
@@ -22,6 +26,44 @@ function App() {
     fetchData()
   }, [])
 
+  const handleChangeForm = (e) => {
+    const { name, value } = e.target
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }))
+  }
+
+  const handleSubmitForm = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.post(URLAPI, formData)
+      data.push(response.data)
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
+  const handleDeleteForm = async (id) => {
+    try {
+      await axios.delete(`${URLAPI}/${id}`)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  const handleSetUpdateForm = (id) => {
+    setFormData(data.find((client) => client.id === id))
+  }
+
+  const handleUpdateForm = async () => {
+    try {
+      const response = await axios.put(`${URLAPI}/${id}`, formData)
+      data.push(response.data)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
 
   return (
     <>
@@ -32,28 +74,28 @@ function App() {
       </header>
       <main className='flex items-center justify-center gap-10'>
         <section className='w-full'>
-          <form action="" className="w-md p-8 rounded-xl flex flex-col items-center justify-center gap-6 bg-gray-100 shadow-gray-200 shadow-xl">
-            <label htmlFor="" className="w-full flex flex-col items-start justify-center gap-2">
+          <form  onSubmit={handleSubmitForm} className="w-md p-8 rounded-xl flex flex-col items-center justify-center gap-6 bg-gray-100 shadow-gray-200 shadow-xl">
+            <label className="w-full flex flex-col items-start justify-center gap-2">
               Nombre completo
-              <input className='p-2 w-full border-2 border-solid border-blue-400 rounded-lg' type="text" name="Nombre completo" id="" />
+              <input className='p-2 w-full border-2 border-solid border-blue-400 rounded-lg' type="text" name="name" value={formData.name} onChange={handleChangeForm} />
             </label>
-            <label htmlFor="" className="w-full flex flex-col items-start justify-center gap-2">
+            <label className="w-full flex flex-col items-start justify-center gap-2">
               Email
-              <input className='p-2 w-full border-2 border-solid border-blue-400 rounded-lg' type="email" name="" id="" />
+              <input className='p-2 w-full border-2 border-solid border-blue-400 rounded-lg' type="email" name="email" value={formData.email} onChange={handleChangeForm} />
             </label>
-            <label htmlFor="" className="w-full flex flex-col items-start justify-center gap-2">
+            <label className="w-full flex flex-col items-start justify-center gap-2">
               Teléfono
-              <input className='p-2 w-full border-2 border-solid border-blue-400 rounded-lg' type="tel" name="" id="" />
+              <input className='p-2 w-full border-2 border-solid border-blue-400 rounded-lg' type="tel" name="phoneNumber" value={formData.phoneNumber} onChange={handleChangeForm} />
             </label>
-            <label htmlFor="" className="w-full flex flex-col items-start justify-center gap-2">
+            <label className="w-full flex flex-col items-start justify-center gap-2">
               Empresa
-              <input className='p-2 w-full border-2 border-solid border-blue-400 rounded-lg' type="text" name="" id="" />
+              <input className='p-2 w-full border-2 border-solid border-blue-400 rounded-lg' type="text" name="enterprise" value={formData.enterprise} onChange={handleChangeForm} />
             </label>
             <div className='w-full flex flex-col items-center justify-center gap-4'>
-              <button className='bg-blue-500 text-xl text-white rounded-xl py-2 w-full' type="button">
+              <button className='bg-blue-500 text-xl text-white rounded-xl py-2 w-full cursor-pointer hover:bg-blue-700 transition-all ease-out duration-200' type="submit">
                 Crear
               </button>
-              <button className='bg-green-500 text-xl text-white rounded-xl py-2 w-full' type="button">
+              <button onClick={handleUpdateForm} className='bg-green-500 text-xl text-white rounded-xl py-2 w-full cursor-pointer hover:bg-green-700 transition-all ease-out duration-200' type="button">
                 Actualizar
               </button>
             </div>
@@ -77,24 +119,6 @@ function App() {
               </TableRow>
             </TableHead>
             <TableBody className="divide-y">
-              <TableRow className="bg-gray-100">
-                <TableCell className="py-6">
-                  Marcos
-                </TableCell>
-                <TableCell>marcos@gmail.com</TableCell>
-                <TableCell>999 999 999</TableCell>
-                <TableCell>Amazon</TableCell>
-                <TableCell>
-                  <a href="#" className="font-medium text-blue-600 hover:underline pr-6">
-                    Edit
-                  </a>
-                </TableCell>
-                <TableCell>
-                  <a href="#" className="font-medium text-red-600 hover:underline pr-6">
-                    Borrar
-                  </a>
-                </TableCell>
-              </TableRow>
               {data.map((row, index) => (
                 <TableRow key={index} className="bg-gray-100">
                   <TableCell className="py-6">
@@ -104,14 +128,14 @@ function App() {
                   <TableCell>{row.phoneNumber}</TableCell>
                   <TableCell>{row.enterprise}</TableCell>
                   <TableCell>
-                    <a href="#" className="font-medium text-blue-600 hover:underline pr-6">
+                    <button onClick={() => handleSetUpdateForm(row.id)} className="font-medium text-blue-600 hover:underline pr-6 cursor-pointer">
                       Edit
-                    </a>
+                    </button>
                   </TableCell>
                   <TableCell>
-                    <a href="#" className="font-medium text-red-600 hover:underline pr-6">
+                    <button onClick={() => handleDeleteForm(row.id)} className="font-medium text-red-600 hover:underline pr-6 cursor-pointer">
                       Borrar
-                    </a>
+                    </button>
                   </TableCell>
                 </TableRow>
               ))}
